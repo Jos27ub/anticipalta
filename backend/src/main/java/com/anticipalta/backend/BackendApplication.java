b7c39259935638bf4fc415a3717b4d426e0557e9
Your Name <you@example.com>
2026-05-31 22:15:58 -0500

    actualizacion backend

--- backend/src/main/java/com/anticipalta/backend/BackendApplication.java
+++ backend/src/main/java/com/anticipalta/backend/BackendApplication.java
@@ -1,13 +1,22 @@
 package com.anticipalta.backend;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
+    @Bean
+    public Hibernate6Module hibernate6Module() {
+        Hibernate6Module module = new Hibernate6Module();
+        module.disable(Hibernate6Module.Feature.USE_TRANSIENT_ANNOTATION);
+        return module;
+    }
 }
