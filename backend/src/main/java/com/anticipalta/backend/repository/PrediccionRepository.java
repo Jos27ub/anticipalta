fabecea7dfc90249539bdd6e1c371abb941f0874
Your Name <you@example.com>
2026-06-01 10:52:15 -0500

    mejorando historial

--- backend/src/main/java/com/anticipalta/backend/repository/PrediccionRepository.java
+++ backend/src/main/java/com/anticipalta/backend/repository/PrediccionRepository.java
@@ -3,6 +3,7 @@ package com.anticipalta.backend.repository;
 import com.anticipalta.backend.entity.Prediccion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 @Repository
