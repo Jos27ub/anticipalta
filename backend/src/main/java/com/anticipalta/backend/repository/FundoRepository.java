fd9f33cbd48959131058cafcd7ed8ea04d3c8511
Your Name <you@example.com>
2026-05-31 23:09:30 -0500

    actualizacion index2

--- backend/src/main/java/com/anticipalta/backend/repository/FundoRepository.java
+++ backend/src/main/java/com/anticipalta/backend/repository/FundoRepository.java
@@ -2,11 +2,10 @@ package com.anticipalta.backend.repository;
 
 import com.anticipalta.backend.entity.Fundo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
-@Repository
 public interface FundoRepository extends JpaRepository<Fundo, Long> {
     @Query("SELECT f FROM Fundo f JOIN FETCH f.provincia JOIN FETCH f.usuario")
-        List<Fundo> findAllWithRelations();
+    List<Fundo> findAllWithRelations();
 }
